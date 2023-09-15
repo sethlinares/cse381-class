355cf32347f2d93a0745c02b1fda4d9e75d8b5c8
sethlinares <104448468+sethlinares@users.noreply.github.com>
2023-09-15 12:13:58 -0600

    Prove Assignment 1

--- AlgorithmLib/BetterLinearSearch.cs
+++ AlgorithmLib/BetterLinearSearch.cs
@@ -1,10 +1,21 @@
 ﻿namespace AlgorithmLib;
 
-public static class BetterLinearSearch
-{
-    public static int Search(List<IComparable> data, IComparable target)
-    {
-        // ADD CODE HERE AND FIX RETURN STATEMENT
+public static class BetterLinearSearch {
+    public static int Search(List<IComparable> data, IComparable target) {
+
+        // For loop that iterates through the list 'data'.
+        for(int i = 0; i < data.Count; i++) {
+
+            // If statement that checks if the current index of 'data' is equal to the target.
+            if(data[i].Equals(target)) {
+                // if the current index of 'data' is equal to the target, return the index.
+                return i;
+            }
+        }
+
+        // If the target is not found, return -1.
         return -1;
     }
+
+    
 }
